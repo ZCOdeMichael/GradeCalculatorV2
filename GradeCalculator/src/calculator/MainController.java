1be0479062172f20b2074bc047ca4a7f6fef84ec
ZCodeMichael <zcodemichael@gmail.com>
2021-01-04 17:58:06 -0500

    Added simple UI

--- GradeCalculator/src/calculator/MainController.java
+++ GradeCalculator/src/calculator/MainController.java
@@ -1,5 +1,35 @@
 package calculator;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class MainController {
-	
+
+    @FXML
+    private ListView<?> weightList;
+
+    @FXML
+    private TextField weight_Input;
+
+    @FXML
+    private Text perc_Remaining, result;
+
+    @FXML
+    void on_Add(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Input(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Remove(ActionEvent event) {
+
+    }
+
 }
